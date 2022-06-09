09ce38ebdb8f2b030cda16b9682159dfe518fab5
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-06-09 18:32:24 +0200

    fix: image url

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,48 +1,51 @@
 import React from "react";
-
+import { ListData } from "./ListData";
 
 function Sidebar() {
     const backgroundLogo = {
-        backgroundImage: `url(/unicorno_logo.png)`,
+        backgroundImage: `url('/unicorno_logo.png')`
     }
     return (
-        <aside className="sidebar border gradient">
+        <nav className="sidebar border gradient">
             <div className="row-wrap align-center">
                 <div className="logo" style={backgroundLogo}></div>
                 <h2 className="font-josefin txt-size-l">Unicorn <br /> Trading</h2>
             </div>
 
-
-            <div className="link-sidebar column justify-between p-l-m p-r-m">
-                <div className="p-b-s border_b w-100">
+            <div className="sidebar-links column justify-between">
+                <div className="p-b-s w-100 sidebar-link">
                     <a href="" className="font-josefin txt-size-m">Why us?</a>
+                    <div className="border_b"></div>
                 </div>
-                <div className="p-b-s border_b w-100">
+                <div className="p-b-s w-100  sidebar-link">
                     <a href="" className="font-josefin txt-size-m">Competitive Advantage of Artificial Intelligence</a>
+                    <div className="border_b"></div>
                 </div>
-                <div className="p-b-s border_b w-100">
+                <div className="p-b-s w-100  sidebar-link">
                     <a href="" className="font-josefin txt-size-m">Speed and time are at our core</a>
+                    <div className="border_b"></div>
                 </div>
-                <div className="p-b-s  border_b w-100">
+                <div className="p-b-s w-100  sidebar-link">
                     <a href="" className="font-josefin txt-size-m">Built in options chain scanning</a>
+                    <div className="border_b"></div>
                 </div>
-                <div className="p-b-s  border_b w-100">
+                <div className="p-b-s w-100  sidebar-link">
                     <a href="" className="font-josefin txt-size-m">Powerful for beginners and seasoned professionals alike</a>
+                    <div className="border_b"></div>
                 </div>
 
             </div>
-            <div className="row-nowrap justify-between">
-                <button className="btn font-quicksand">
-                    Login
+            <div className="nav-btn-container justify-between">
+                <button className="navbtn font-josefin txt-size-l txt-weight-semibold">
+                    <a>Login</a>
+                    <div className="border_b w-100"></div>
                 </button>
-
-                <button className="btn font-quicksand">
-                    Sign Up
+                <button className="navbtn font-josefin txt-size-l txt-weight-semibold">
+                    <a>Sign Up</a>
+                    <div className="border_b w-100"></div>
                 </button>
             </div>
-
-        </aside>
-
+        </nav>
     )
 }
 
