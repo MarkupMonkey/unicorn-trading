92e2b01cd7b90fcb81487573bde9a4a9764cc48a
Marco Moncada <marco.monkey93@gmail.com>
2022-06-09 11:21:14 +0200

    feat: responsive pricing section created

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import { loadFull } from "tsparticles";
 import particlesOptions from "./particles.json";
 
 //hero section
-import {Hero} from './components/Hero';
+import { Hero } from './components/Hero';
 
 // Homepage lists portion
 import Lists from './components/Lists';
 import ListData from './components/ListData';
 
+// Homepage pricing section
+import { Pricing } from './components/Pricing';
+import { PricesWrapper } from './components/PricingWrapper';
+
 function App() {
     const particlesInit = useCallback(main => {
         loadFull(main);
     }, [])
-    
+
     // Iterating through homepage list array
-    const homePageList =ListData.map((item) => {
+    const homePageList = ListData.map((item) => {
         return (
-          <Lists key={item.id} {...item}/>
-      )
-  })
+            <Lists  {...item} />
+        )
+    })
+
+    // const pricing = [
+    //     { id: 1, name: 'month', price: 67 },
+    //     { id: 2, name: 'year', price: 670 }
+    // ]
+
+    // const pricingMenu = pricing.map(items => {
+    //     return (
+    //         <Pricing key={items.id} {...items} />
+    //     )
+    // })
 
     return (
         <div className="App">
@@ -28,9 +43,13 @@ function App() {
             <header className="bg-cottoncandy">
                 <h1 className=''>Hello world!</h1>
             </header>
-           <Hero/> 
+            <Hero />
             {/* Rendering homepage List items */}
             {homePageList}
+            <PricesWrapper>
+                 {/* {pricingMenu} */}
+            </PricesWrapper>
+           
         </div>
     );
 }
---
+++ src/components/Pricing.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Pricing = (props) => {
+    return (
+        <section className="card" >
+            <div className="price" >
+                <h2> Only ${props.price} /{props.name}</h2>
+                <div className="ghost">
+                    {props.name === 'year' ? <p style={{ color: 'coral' }}>SAVE $134 ANNUALLY</p> : ''}
+                </div>
+                <ul>
+                    <li> Daily options recommendation</li>
+                    <li>Suggested strike prices for each symbol</li>
+                    <li>8 recommended options strategies</li>
+                    <li> Grading system for better options trading</li>
+                    <li>Proprietary indicators</li>
+                    <input className="pricing-btn1" type='submit' value='Subscribe Now' />
+                </ul>
+            </div>
+        </section>
+    )
+}
